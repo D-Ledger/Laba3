db2df87076815e45c2384098d104682f7dc6e491
Danil <danil.melnikov1404@gmail.com>
2025-12-07 15:20:37 +0500

    Add files via upload

---
+++ src/fp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class fp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = 10;
+        do {
+            System.out.println("раз" + a);
+            a--;
+        }
+        while (a > 10);
+    }
+}
